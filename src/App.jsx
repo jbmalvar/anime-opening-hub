e4db30bad27c604a6daf6c0d0cde6c51e43a0d9c
Yeli.James <jbmalvar.cs.washington.edu>
2025-04-22 04:33:34 -0700

    Day 1 End

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import { useState } from 'react'
+import { Link, Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
+
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className="App">
+      <nav className="topBar">
+        <div className="structureFlex">
+          <div className="topTitle">
+            <span style={{ color: 'white' }}>AnimeOp</span>
+            <span style={{ color: 'red' }}>Hub</span>
+          </div>
+          <div className="rightSide">
+            <a>Search</a>
+            <a>Create</a>
+            <a>Animes</a>
+          </div>
+        </div>
+      </nav>
+      <div className="main">
+        <Routes>
+          
+        </Routes>
+      </div>
+    </div>
   )
 }
 
